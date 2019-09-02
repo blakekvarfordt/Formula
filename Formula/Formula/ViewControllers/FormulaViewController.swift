2f4836edb83120c119064255c2d87b0b48e0488b
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-09-02 16:04:39 -0600

    starts project over

---
+++ Formula/Formula/ViewControllers/FormulaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormulaViewController.swift
+//  Formula
+//
+//  Created by Blake kvarfordt on 9/2/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class FormulaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
