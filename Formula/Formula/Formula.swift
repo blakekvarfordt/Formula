8dc7198315b4c30c84a1593f520ba5a08f800e34
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-09-02 14:25:01 -0600

    sets up model

---
+++ Formula/Formula/Formula.swift
@@ -0,0 +1,19 @@
+//
+//  Formula.swift
+//  Formula
+//
+//  Created by Blake kvarfordt on 9/2/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+class Formula {
+    let title: String
+    let number: Int
+    
+    init(title: String, number: Int) {
+        self.title = title
+        self.number = number
+    }
+}
